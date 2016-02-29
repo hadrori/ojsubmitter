9c5547da17da4f8be13997d04e9fd329533d959c
hadrori <hadrori.hs@gmail.com>
2016-02-29 17:51:33 +0900

    Update spec

--- spec/ojsubmitter/judge_spec.rb
+++ spec/ojsubmitter/judge_spec.rb
@@ -14,6 +14,26 @@ module OJS
       end
     end
 
+    describe "#submit" do
+      let(:option) {
+        { 'judge' => 'judge',
+          'user'  => 'alice',
+          'password' => 'pswd',
+          'language' => 'lang' }
+      }
+      before do
+        allow(Judge).to receive(:login)
+        allow(Judge).to receive(:post)
+        allow(Judge).to receive(:open_status)
+        allow_any_instance_of(Object).to receive(:sleep)
+      end
+
+      it "sets config" do
+        Judge.submit option
+        expect(Judge.instance_variable_get(:@config)).to eq(option)
+      end
+    end
+
     describe "#post" do
       subject(:method) { Judge.post }
       it_behaves_like 'a not implemented method'
@@ -22,6 +42,48 @@ module OJS
     describe "#open_status" do
       subject(:method) { Judge.open_status }
       it_behaves_like 'a not implemented method'
+    end
+
+    let(:option) {
+      { 'judge'    => 'aoj',
+        'user'     => 'alice',
+        'password' => 'pswd',
+        'language' => 'c++',
+        'file'     => '1234.cpp' }
+    }
+
+    before do
+      Judge.instance_variable_set(:@config, option)
+    end
+
+    describe "#password" do
+      it 'shows password' do
+        expect(Judge.password).to eq 'pswd'
+      end
+    end
+
+    describe "#language" do
+      it 'shows language' do
+        expect(Judge.language).to eq 'c++'
+      end
+    end
+
+    describe "#problem_id" do
+      context 'not specified problem_id' do
+        it 'will be nil' do
+          expect(Judge.problem_id).to be nil
+        end
+      end
+
+      context 'specified problem_id' do
+        before do
+          option['problem_id'] = '1234'
+        end
+
+        it 'uses problem_id directly' do
+          expect(AOJ.problem_id).to eq '1234'
+        end
+      end
     end
   end
 end
